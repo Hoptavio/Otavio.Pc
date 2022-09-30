4438e823900b1b6eff79663a5dad286c019c669b
Hoptavio <ohoptavio@gmail.com>
2022-09-30 14:08:53 -0300

    Barra rotativa pronta

--- Carrossel/carrossel.js
+++ Carrossel/carrossel.js
@@ -1,13 +1,16 @@
+// Criando as variaveis para fazer a seleção das imagens
 const controls = document.querySelectorAll(".control");
 let currentItem = 0;
 const items = document.querySelectorAll(".item");
-const maxItens = items.length;
+const maxItens = items.length; 
+// Fim Criando as variaveis para fazer a seleção das imagens
 
+//Selecionando a imagem principal atraves das setas
 controls.forEach((control) => {
     control.addEventListener("click", () =>{
         
         const isleft = control.classList.contains("arrow-left"); 
-
+       //Criando um looping 
        if(isleft)
        {
          currentItem -= 1;
@@ -24,15 +27,19 @@ controls.forEach((control) => {
        {
          currentItem = maxItens -1;
        }
+       // Fim Criando um looping 
       
+       //Definindo qual e a imagem selecionada
        items.forEach(item => item.classList.remove("atual"));
-
+       //Suavizando e centralizando a imagem selecionada
        items[currentItem].scrollIntoView({
         
          inline: "center",
          behavior: "smooth"
        });
-
+      // Fim Suavizando e centralizando a imagem selecionada
        items[currentItem].classList.add("atual");
+       // Fim Definindo qual e a imagem selecionada
     });
 });
+// Fim Selecionando a imagem principal atraves das setas
